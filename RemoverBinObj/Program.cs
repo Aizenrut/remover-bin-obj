d1caa3058da867a129dfe67acf855627ccdc8cf6
Igor Christofer Eisenhut <igor.eisenhut@pagueveloz.com.br>
2022-02-11 16:19:05 -0300

    Commit inicial

---
+++ RemoverBinObj/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Diretórios removidos:");
+Console.WriteLine();
+
+foreach (var dir in Directory.GetDirectories(@"C:\Projetos\PagueVeloz"))
+{
+    foreach (var obj in Directory.GetDirectories(dir, "obj"))
+    {
+        Console.WriteLine(obj);
+        Directory.Delete(obj, true);    
+    }
+
+    foreach (var bin in Directory.GetDirectories(dir, "bin"))
+    {
+        Console.WriteLine(bin);
+        Directory.Delete(bin, true);
+    }
+}
